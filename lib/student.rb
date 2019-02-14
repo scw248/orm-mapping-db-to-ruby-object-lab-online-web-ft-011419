b592e050168e7dedb9e4503de169c122909ba6cb
Stephen Wright <scw248@gmail.com>
2019-02-14 18:11:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,7 +68,17 @@ class Student
     end.first
   end
   
-  
+    def self.all_students_in_grade_x(grade)
+     sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade = '#{grade}'
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
   def self.find_by_name(name)
     sql = <<-SQL
